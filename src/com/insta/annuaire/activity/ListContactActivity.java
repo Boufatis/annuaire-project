0786c3ed960762b9ceb37de1b8d2530b7b8cca11
Romaric BOUFATIS <r.boufatis@insta.fr>
2015-04-22 09:35:41 +0200

    Add search !

--- src/com/insta/annuaire/activity/ListContactActivity.java
+++ src/com/insta/annuaire/activity/ListContactActivity.java
@@ -6,6 +6,7 @@ import com.insta.annuaire.dao.DAOContact;
 import com.insta.gen.annuaire.R;
 import com.squareup.picasso.Picasso;
 
+import Enum.EnumOrder;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
@@ -13,6 +14,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,49 +30,110 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
-public class ListContactActivity extends ListActivity{
+public class ListContactActivity extends Activity{
 	
-	
-	public static ListContactActivity context;
+	private ListView lvContact;
+	ContactAdapter adaptateur;
+	EditText inputSearch;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.listcontact);
         DAOContact daoContact = new DAOContact();
-        ContactAdapter adaptateur = new ContactAdapter(this,daoContact.getListContact());
-        setListAdapter(adaptateur);
-	}
-	
-	@Override
-	protected void onListItemClick (ListView l, View v, int position, long id) {
-		
-		String contactid = ((TextView)v.findViewById(R.id.hiddenId)).getText().toString();
-		int idContact = Integer.parseInt(contactid);
-		DAOContact daoContact = new DAOContact();
-		Contact contact = new Contact();
-		try {
-			contact = daoContact.getContactById(idContact);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-//		Toast.makeText(ListContactActivity.this, contact.getNom(), Toast.LENGTH_SHORT).show();
-        LayoutInflater factory = LayoutInflater.from(this);
-        final View alertDialogView = factory.inflate(R.layout.showcontact, null);
-        AlertDialog.Builder adb = new AlertDialog.Builder(this);
-        adb.setView(alertDialogView);
-        adb.setTitle(contact.getPrenom()+ " "+contact.getNom().toUpperCase());       
-        ImageView photo = (ImageView)alertDialogView.findViewById(R.id.photoContact);
-        if(!contact.getPhoto().isEmpty())
-        {
-			Picasso.with(this).load(contact.getPhoto()).transform(new CircleTransform()).into(photo);
-        }else{
-        	Picasso.with(this).load(R.drawable.user).transform(new CircleTransform()).into(photo);
-        }
-        //adb.setIcon(android.R.drawable.ic_dialog_alert);
-        adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
+        lvContact = (ListView)findViewById(R.id.listViewContact);
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        adaptateur = new ContactAdapter(this,daoContact.getListContact());
+        lvContact.setAdapter(adaptateur);
+        lvContact.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView adapterView, View view,int position, long id) 
+			{
+				String  contactid = ((TextView)view.findViewById(R.id.hiddenId)).getText().toString();
+				int idContact = Integer.parseInt(contactid);
+				DAOContact daoContact = new DAOContact();
+				Contact contact = new Contact();
+				try {
+					contact = daoContact.getContactById(idContact);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+//				Toast.makeText(ListContactActivity.this, contact.getNom(), Toast.LENGTH_SHORT).show();
+		        LayoutInflater factory = LayoutInflater.from(ListContactActivity.this);
+		        final View alertDialogView = factory.inflate(R.layout.showcontact, null);
+		        AlertDialog.Builder adb = new AlertDialog.Builder(ListContactActivity.this);
+		        adb.setView(alertDialogView);
+		        adb.setTitle(contact.getPrenom()+ " "+contact.getNom().toUpperCase());       
+		        ImageView photo = (ImageView)alertDialogView.findViewById(R.id.photoContact);
+		        if(!contact.getPhoto().isEmpty())
+		        {
+					Picasso.with(ListContactActivity.this).load(contact.getPhoto()).transform(new CircleTransform()).into(photo);
+		        }else{
+		        	Picasso.with(ListContactActivity.this).load(R.drawable.user).transform(new CircleTransform()).into(photo);
+		        }
+		        //adb.setIcon(android.R.drawable.ic_dialog_alert);
+		        adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		            public void onClick(DialogInterface dialog, int which) {
 
-          } });
-        adb.show();
+		          } });
+		        adb.show();
+			}
+        });
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+            	lvContact = (ListView)findViewById(R.id.listViewContact);
+                inputSearch = (EditText) findViewById(R.id.inputSearch);
+                DAOContact daoContact = new DAOContact();
+                adaptateur = new ContactAdapter(ListContactActivity.this,daoContact.getListContactByCriteria(cs.toString(), "all", EnumOrder.ASC));
+                lvContact.setAdapter(adaptateur);
+                lvContact.setOnItemClickListener(new OnItemClickListener() {
+        			public void onItemClick(AdapterView adapterView, View view,int position, long id) 
+        			{
+        				String  contactid = ((TextView)view.findViewById(R.id.hiddenId)).getText().toString();
+        				int idContact = Integer.parseInt(contactid);
+        				DAOContact daoContact = new DAOContact();
+        				Contact contact = new Contact();
+        				try {
+        					contact = daoContact.getContactById(idContact);
+        				} catch (Exception e) {
+        					e.printStackTrace();
+        				}
+//        				Toast.makeText(ListContactActivity.this, contact.getNom(), Toast.LENGTH_SHORT).show();
+        		        LayoutInflater factory = LayoutInflater.from(ListContactActivity.this);
+        		        final View alertDialogView = factory.inflate(R.layout.showcontact, null);
+        		        AlertDialog.Builder adb = new AlertDialog.Builder(ListContactActivity.this);
+        		        adb.setView(alertDialogView);
+        		        adb.setTitle(contact.getPrenom()+ " "+contact.getNom().toUpperCase());       
+        		        ImageView photo = (ImageView)alertDialogView.findViewById(R.id.photoContact);
+        		        if(!contact.getPhoto().isEmpty())
+        		        {
+        					Picasso.with(ListContactActivity.this).load(contact.getPhoto()).transform(new CircleTransform()).into(photo);
+        		        }else{
+        		        	Picasso.with(ListContactActivity.this).load(R.drawable.user).transform(new CircleTransform()).into(photo);
+        		        }
+        		        //adb.setIcon(android.R.drawable.ic_dialog_alert);
+        		        adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        		            public void onClick(DialogInterface dialog, int which) {
+
+        		          } });
+        		        adb.show();
+        			}
+                });
+            }
+             
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                    int arg3) {
+                // TODO Auto-generated method stub
+                 
+            }
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
 	}
 }
--- src/com/insta/annuaire/dao/DAOContact.java
+++ src/com/insta/annuaire/dao/DAOContact.java
@@ -1,5 +1,6 @@
 package com.insta.annuaire.dao;
 
+import java.io.Console;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,6 +11,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import Enum.EnumOrder;
 
 import com.insta.annuaire.Contact;
 
@@ -46,6 +49,43 @@ public class DAOContact extends DAO {
 	    return contacts;
 	}
 	
+	public List<Contact> getListContactByCriteria(String search, String typeUser, EnumOrder order){
+		
+		if(search.isEmpty()){return this.getListContact();}
+		
+		String orderby;
+		if(!typeUser.equals("etudiant") || !typeUser.equals("professeur")){
+			typeUser = "all";
+		}
+		if(order == EnumOrder.ASC) { orderby ="ASC";}else{orderby ="DESC";}
+		System.out.println("RECHERCHE : " + search + " typeUser : " + " oderby : " + orderby);
+ 		this.URL = "http://dev-project.it:3000/annuaire/get/" + search + "/" + typeUser + "/" + orderby;
+		super.setURL(URL);
+        // prepare the list of all records        
+		List<Contact> contacts = new ArrayList<Contact>();
+        JSONArray jArray;
+		try 
+		{
+			JSONObject jobject = new JSONObject(super.readResult());
+			jArray = jobject.getJSONArray("profils");
+			for(int i = 0; i < jArray.length(); i++){
+	        	JSONObject json_data = jArray.getJSONObject(i);	
+	        	Contact contact = new Contact();
+	        	contact.setId(json_data.getInt("id"));
+	        	contact.setNom(json_data.getString("nom"));
+	        	contact.setPrenom(json_data.getString("prenom"));
+	        	contact.setProfil(json_data.getString("profil"));
+	        	contact.setPromo(json_data.getString("promo"));
+	        	contact.setPhoto(json_data.getString("photo"));
+	        	contacts.add(contact);       
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	       
+	    return contacts;
+	}
+	
 	public Contact getContactById(int id) throws Exception{
 		
 		this.URL = "http://dev-project.it:3000/annuaire/get/"+id;
@@ -66,6 +106,7 @@ public class DAOContact extends DAO {
 			contact.setMail(json_data.getString("mail"));
 			contact.setTelephone(json_data.getString("telephone"));
 			contact.setPhoto(json_data.getString("photo"));
+			
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}	       
